81f7c904aa65902d4685290e627bf8ab700d1c07
Menno <run_jongejan@hotmail.com>
2020-09-07 19:39:20 +0200

    Added question data class, Added questionsadapter, created question layout, created main layout

--- app/src/main/java/com/example/android_task_22/MainActivity.kt
+++ app/src/main/java/com/example/android_task_22/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.android_task_22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_task_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+
+    // Don't forget to create a binding object as you did in previous assignments.
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+
+    private fun initViews() {
+        binding.rvQuestions.layoutManager =
+                LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        //createItemTouchHelper().attachToRecyclerView(binding.rvReminders)
     }
 }
---
+++ app/src/main/java/com/example/android_task_22/Question.kt
@@ -0,0 +1,6 @@
+package com.example.android_task_22
+
+data class Question(
+    var question:String,
+    var isTrue:Boolean
+)
